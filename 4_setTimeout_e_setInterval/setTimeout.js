8bd816f1d19de34b9e6b77778a40a3015555cde0
Eduardo Luiz Zanotto <91149760+Eduardo-Luizz@users.noreply.github.com>
2023-06-12 13:17:51 -0300

    Aprendendo sobre setTimeou e setInterval

---
+++ 4_setTimeout_e_setInterval/setTimeout.js
@@ -0,0 +1,13 @@
+// console.log('Programa iniciado!')
+
+// const timeoutId = setTimeout(() => {
+//     console.log('2 segundos se passaram desde que o programa foi iniciado.')
+// }, 2 * 1000)
+
+console.log('Programa iniciado!')
+
+const timeoutId = setTimeout(() => {
+    console.log('2 segundos se passaram desde que o programa foi iniciado.')
+}, 2 * 1000)
+
+clearTimeout(timeoutId)
